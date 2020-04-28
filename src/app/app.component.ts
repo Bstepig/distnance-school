42c3eaf7437e7621b8a59c2528e4c29926ba95c1
Danilov Timofey <volinadtim@gmail.com>
2020-04-28 15:00:20 +0300

    translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'ds-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'distance-school';
+  constructor(private _translate: TranslateService) {
+    _translate.addLangs(['en', 'ru']);
+    _translate.setDefaultLang('ru');
+
+    const browserLang = _translate.getBrowserLang();
+    _translate.use(browserLang.match(/en|ru/) ? browserLang : 'ru');
+  }
 }
